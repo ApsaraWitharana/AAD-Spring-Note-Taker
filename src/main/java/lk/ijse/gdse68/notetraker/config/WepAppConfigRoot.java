4354767008c042064aef2b78563684a44e28583f
ApsaraWitharana <asachini095@gmail.com>
2024-08-25 15:28:15 +0530

    update-confic add to ORM

--- src/main/java/lk/ijse/gdse68/notetraker/config/WepAppConfigRoot.java
+++ src/main/java/lk/ijse/gdse68/notetraker/config/WepAppConfigRoot.java
@@ -1,14 +1,58 @@
 package lk.ijse.gdse68.notetraker.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages ="lk.ijse.gdse68.notetraker" )
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class WepAppConfigRoot {
-}
+    @Bean // source for bean definition
+    public DataSource dataSource() {
 
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.HSQL).build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//vendorAdapter -kenekw connect krnwa hybernet connect krl tiyenne
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+//
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.acme.domain");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        //jpl- eken enne -EntityManagerFactory -hibernate connecter
+//translation manage krnn
+ //set orm implementation to this method
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+}
+//db confic -application ek puram web patt natuwa ew manage krnne meke
 //why using web application layer 2 - app config direct blanna  app config// data connection handle root config
 //configuration class - source for bean definition // kohend bean gnne mona package walinda //application eke configuration kohed hadann kiyl //bean is a spring object is a managing bean object
+//bean -spring t manage krnn puluwan objec hdnwa

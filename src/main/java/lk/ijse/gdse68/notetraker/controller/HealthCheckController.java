4b4cf4c01eb08b752c0c53df64c0fa96bcffbec8
ApsaraWitharana <asachini095@gmail.com>
2024-09-29 10:10:13 +0530

    update-healthchek controller set

---
+++ src/main/java/lk/ijse/gdse68/notetraker/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package lk.ijse.gdse68.notetraker.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/healthTest")
+@RequiredArgsConstructor
+public class HealthCheckController {
+    @GetMapping
+    public String healthTest(){
+        return "Note controller run successfully!!";
+    }
+}
